f8248ef81a64be10cf42c3b9ae41fc86cb5e5106
Piotr Walen <walen.piotr@gmail.com>
2026-02-06 14:47:19 +0100

    Update RegisterFeature function to use mqtt.MQTTSubscriber type for improved MQTT integration in the weather module

--- server/internal/modules/weather/register.go
+++ server/internal/modules/weather/register.go
@@ -4,11 +4,12 @@ import (
 	"cloudpico-server/internal/modules/weather/controller"
 	"cloudpico-server/internal/modules/weather/repository"
 	"cloudpico-server/internal/modules/weather/service"
+	"cloudpico-server/internal/mqtt"
 	"database/sql"
 	"net/http"
 )
 
-func RegisterFeature(mux *http.ServeMux, db *sql.DB, subscriber MQTTSubscriber) {
+func RegisterFeature(mux *http.ServeMux, db *sql.DB, subscriber mqtt.MQTTSubscriber) {
 	weatherRepository := repository.NewRepository(db)
 	weatherService := service.NewService(weatherRepository)
 	weatherService.Register(subscriber)
